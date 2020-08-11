cb41490f2c1b67a3fcb4041fdbb984f7ba30c748
guancong <guancong@elensdata.com>
2020-08-11 13:34:13 +0800

    Fix css :memo:

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
   text-align: center;
-  color: #2c3e50;
 }
 </style>
